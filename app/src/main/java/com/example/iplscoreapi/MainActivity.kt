4e0adbcf9d453ee8411e593a1838643e6bb0ba58
sagar <sagaryadav12369@gmail.com>
2024-11-05 19:47:17 +0530

    api

--- app/src/main/java/com/example/iplscoreapi/MainActivity.kt
+++ app/src/main/java/com/example/iplscoreapi/MainActivity.kt
@@ -21,21 +21,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
         val matchApi = ApiUtilities.getInstance().create(ApiInterface::class.java)
-
-
         lifecycleScope.launch(Dispatchers.IO) {
-
             val result = matchApi.getSeries(API_KEY, SERIES_ID)
 
             if (result.body() != null) {
-                Log.d("SHUBH", "onCreate: ${result.body()!!.data.matchList}")
+                Log.d("Sagar", "onCreate: ${result.body()!!.data.matchList}")
 
                 withContext(Dispatchers.Main) {
                     binding.progressbar.visibility = GONE
-                    binding.recyclerView.adapter = MatchAdapter(this@MainActivity, result.body()!!.data.matchList)
+                    binding.recyclerView.adapter =
+                        MatchAdapter(this@MainActivity, result.body()!!.data.matchList)
                 }
             }
         }
